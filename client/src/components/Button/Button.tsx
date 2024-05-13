91ce23ba3398803dcf318db661c34e419a040bde
kexogg <kexogg@gmail.com>
2024-05-13 15:40:03 +0500

    add new styles

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -2,11 +2,14 @@ import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = {
     children: ReactNode;
+    outline?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({children, ...props}: ButtonProps) => {
+const Button = ({children, outline, ...props}: ButtonProps) => {
     return (
-        <button className="transition-colors bg-primary-500 hover:bg-primary-600 text-white font-bold py-2 px-3 rounded" {...props}>
+        <button className={`transition-colors 
+        ${outline ? 'outline outline-1 outline-primary-600 hover:outline-primary-700 hover:text-primary-700 text-primary-600' : 
+            'bg-primary-600 hover:bg-primary-700 text-white'} font-semibold py-1.5 px-3 rounded-md`} {...props}>
             {children}
         </button>
     );
