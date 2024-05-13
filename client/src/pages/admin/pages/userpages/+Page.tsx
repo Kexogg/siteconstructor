7b449c62b58cc08ff38da86533209416f81160e6
kexogg <kexogg@gmail.com>
2024-05-13 18:54:30 +0500

    add dialog when creating new page

--- client/src/pages/admin/pages/userpages/+Page.tsx
+++ client/src/pages/admin/pages/userpages/+Page.tsx
@@ -4,9 +4,13 @@ import {useData} from "vike-react/useData";
 import {Data} from "./+data";
 import {navigate} from "vike/client/router";
 import Button from "../../../../components/Button/Button";
+import Dialog from "../../../../components/Dialog/Dialog";
+import Input from "../../../../components/Input/Input";
+import {useState} from "react";
 
 const Page = () => {
     const data = useData<Data>()
+    const [dialogOpen, setDialogOpen] = useState(false)
     return (
         <AdminPageContainer title="Страницы">
             <p>В этом разделе вы можете управлять страницами сайта</p>
@@ -26,15 +30,33 @@ const Page = () => {
                 }}/>
                 <div className='ms-auto mt-3 w-fit'>
                     <span className={'mr-3'}>Количество элементов: {data.length}</span>
-                    <Button outline>Добавить страницу</Button>
+                    <Button outline onClick={() => setDialogOpen(true)}>Добавить страницу</Button>
                 </div>
             </div>
             <div className={'flex gap-3'}>
                 <Button>Сохранить</Button>
                 <Button outline>Удалить</Button>
             </div>
+            <NewPageDialog open={dialogOpen} onClose={() => setDialogOpen(false)}/>
         </AdminPageContainer>
     );
 };
+
+type NewPageDialogProps = {
+    open: boolean;
+    onClose: () => void;
+}
+
+const NewPageDialog = ({open, onClose}: NewPageDialogProps) => {
+    return (
+        <Dialog open={open} onClose={onClose} title={'Добавить страницу'}>
+            <div className={'flex flex-col gap-3'}>
+                <Input placeholder={'Название'}/>
+                <Input placeholder={'Адрес'}/>
+                <Button onClick={() => navigate('/admin/userpages/new')}>Добавить</Button>
+            </div>
+        </Dialog>
+    )
+}
 
 export default Page;
--- client/src/pages/admin/pages/userpages/@id/+Page.tsx
+++ client/src/pages/admin/pages/userpages/@id/+Page.tsx
@@ -7,46 +7,75 @@ import AdminEditorSection from "../../../../../components/Admin/AdminEditor/Admi
 import AdminEditorItem from "../../../../../components/Admin/AdminEditor/AdminEditorItem";
 import Input from "../../../../../components/Input/Input";
 import {navigate} from "vike/client/router";
+import Dialog from "../../../../../components/Dialog/Dialog";
+import {useState} from "react";
+import Select from "../../../../../components/Select/Select";
 
 const Page = () => {
     const data = useData<Data>()
+    const [dialogOpen, setDialogOpen] = useState(false)
     return (
-        <AdminPageContainer title={`Страница "${data.title}"`}>
-            <pre>ID {data.id}</pre>
-            <AdminEditorSection title={'Настройки страницы'}>
-                <AdminEditorItem label={'Название'}>
-                    <Input defaultValue={data.title}/>
-                </AdminEditorItem>
-                <AdminEditorItem label={'Описание'}>
-                    <Input defaultValue={data.description}/>
-                </AdminEditorItem>
-                <AdminEditorItem label={'Адрес'}>
-                    <Input defaultValue={data.pageUrl}/>
-                </AdminEditorItem>
-            </AdminEditorSection>
-            <h2 className={"text-xl font-bold my-3"}>Блоки</h2>
-            <div className={'flex flex-col'}>
-                <AdminTable data={data.blocks} columns={[
-                    {key: "id", title: "ID", isNarrow: true},
-                    {key: "type", title: "Тип"}
-                ]} actions={{
-                    edit: (id) => {
-                        navigate('/admin/userpages/' + data.id + '/blocks/' + id)
-                    },
-                    delete: (id) => {
-                        console.log('Deleting block ' + id)
-                    },
-                }}/>
-                <div className='ms-auto mt-3 w-fit'>
-                    <span className={'mr-3'}>Количество элементов: {data.blocks.length}</span><Button outline>Добавить блок</Button>
+        <>
+            <AdminPageContainer title={`Страница "${data.title}"`}>
+                <pre>ID {data.id}</pre>
+                <AdminEditorSection title={'Настройки страницы'}>
+                    <AdminEditorItem label={'Название'}>
+                        <Input defaultValue={data.title}/>
+                    </AdminEditorItem>
+                    <AdminEditorItem label={'Описание'}>
+                        <Input defaultValue={data.description}/>
+                    </AdminEditorItem>
+                    <AdminEditorItem label={'Адрес'}>
+                        <Input defaultValue={data.pageUrl}/>
+                    </AdminEditorItem>
+                </AdminEditorSection>
+                <h2 className={"text-xl font-bold my-3"}>Блоки</h2>
+                <div className={'flex flex-col'}>
+                    <AdminTable data={data.blocks} columns={[
+                        {key: "id", title: "ID", isNarrow: true},
+                        {key: "type", title: "Тип"}
+                    ]} actions={{
+                        edit: (id) => {
+                            navigate('/admin/userpages/' + data.id + '/blocks/' + id)
+                        },
+                        delete: (id) => {
+                            console.log('Deleting block ' + id)
+                        },
+                    }}/>
+                    <div className='ms-auto mt-3 w-fit'>
+                        <span className={'mr-3'}>Количество элементов: {data.blocks.length}</span><Button
+                        onClick={() => setDialogOpen(true)} outline>Добавить блок</Button>
+                    </div>
                 </div>
-            </div>
-            <div className={'flex gap-3'}>
-                <Button>Сохранить</Button>
-                <Button outline>Удалить</Button>
-            </div>
-        </AdminPageContainer>
+                <div className={'flex gap-3'}>
+                    <Button>Сохранить</Button>
+                    <Button outline>Удалить</Button>
+                </div>
+            </AdminPageContainer>
+            <NewBlockDialog open={dialogOpen} onClose={() => setDialogOpen(false)}/>
+        </>
     );
 };
+
+type NewBlockDialogProps = {
+    open: boolean;
+    onClose: () => void;
+}
+
+const NewBlockDialog = ({open, onClose}: NewBlockDialogProps) => {
+    return (
+        <Dialog open={open} onClose={onClose} title={'Добавить блок'}>
+            <div className={'flex flex-col gap-3'}>
+                <Input placeholder={'Название'}/>
+                <Select>
+                    <option>Текст</option>
+                    <option>Картинка</option>
+                    <option>Кнопка</option>
+                </Select>
+                <Button onClick={() => navigate('/admin/userpages/TODO/blocks/new')}>Добавить</Button>
+            </div>
+        </Dialog>
+    )
+}
 
 export default Page;
