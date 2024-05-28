da6e73c32a22b520c680ab9beed423d3a4439f23
kexogg <kexogg@gmail.com>
2024-05-28 17:46:04 +0500

    registration logic

--- client/src/pages/admin/pages/login/+Page.tsx
+++ client/src/pages/admin/pages/login/+Page.tsx
@@ -1,20 +1,43 @@
 import Button from "../../../../components/Button/Button";
 import {Link} from "../../../../components/Link";
 import Input from "../../../../components/Input/Input";
-import {navigate} from "vike/client/router";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {reload} from "vike/client/router";
 
 const Page = () => {
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        await fetch('/_auth/login')
-        await navigate('/admin')
+    interface Inputs {
+        login: string,
+        password: string
     }
+
+    const { register, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const response = await fetch('/api/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                login: data.login,
+                password: data.password,
+            }),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            await reload();
+            console.log(result);
+        } else {
+            console.error('Authentication failed');
+        }
+    };
     return (
-        <form className="flex flex-col gap-3" onSubmit={onSubmit}>
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
             <h2 className="text-xl text-center">Авторизация</h2>
-            <Input placeholder="E-mail" required type="email"/>
-            <Input placeholder="Пароль" required type="password"/>
+            <Input {...register('login')} placeholder="E-mail" required type="email"/>
+            <Input {...register('password')} placeholder="Пароль" required type="password"/>
             <Button>Войти</Button>
             <p className='text-center'>Нет аккаунта? <Link href={"/admin/register"}><span
                 className="text-blue-500 underline">Регистрация</span></Link>
--- client/src/pages/admin/pages/register/+Page.tsx
+++ client/src/pages/admin/pages/register/+Page.tsx
@@ -1,22 +1,53 @@
+import {SubmitHandler, useForm} from "react-hook-form";
+import {reload} from "vike/client/router";
 import Input from "../../../../components/Input/Input";
 import Button from "../../../../components/Button/Button";
 import {Link} from "../../../../components/Link";
 
 const Page = () => {
-        return (
-            <form className="flex flex-col gap-3">
-                <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
-                <h2 className="text-xl text-center">Регистрация</h2>
-                <Input placeholder="E-mail" required type="email"/>
-                <Input placeholder="Название компании" required type="text"/>
-                <Input placeholder="Пароль" required type="password"/>
-                <Input placeholder="Повторите пароль" required type="password"/>
-                <Button>Зарегестрироватся</Button>
-                <p className='text-center'>Есть аккаунт? <Link href={"/admin/login"}><span
-                    className="text-blue-500 underline">Войти</span></Link>
-                </p>
-            </form>
-        );
+    interface Inputs {
+        email: string,
+        companyName: string,
+        password: string
+    }
+
+    const {register, handleSubmit} = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: data.email,
+                orgName: data.companyName,
+                password: data.password,
+            }),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            await reload();
+            console.log(result);
+        } else {
+            console.error('Registration failed');
+        }
+    };
+
+    return (
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
+            <h2 className="text-xl text-center">Регистрация</h2>
+            <Input {...register('email')} placeholder="E-mail" required type="email"/>
+            <Input {...register('companyName')} placeholder="Название компании" required type="text"/>
+            <Input {...register('password')} placeholder="Пароль" required type="password"/>
+            <Button>Зарегестрироватся</Button>
+            <p className='text-center'>Есть аккаунт? <Link href={"/admin/login"}><span
+                className="text-blue-500 underline">Войти</span></Link>
+            </p>
+        </form>
+    );
 };
 
 export default Page;
