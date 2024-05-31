c49cdec3f312f9906dc0b9c0ff4d02166080656a
kexogg <kexogg@gmail.com>
2024-06-01 04:16:13 +0500

    fix form

--- client/src/pages/admin/pages/register/+Page.tsx
+++ client/src/pages/admin/pages/register/+Page.tsx
@@ -3,13 +3,13 @@ import Input from "../../../../components/Input/Input";
 import Button from "../../../../components/Button/Button";
 import {Link} from "../../../../components/Link";
 import {useState} from "react";
-import {reload} from "vike/client/router";
 
 const Page = () => {
     const [error, setError] = useState<string | null>(null);
 
     interface Inputs {
         login: string,
+        siteName: string,
         companyName: string,
         password: string
     }
@@ -26,6 +26,7 @@ const Page = () => {
                 login: data.login,
                 orgName: data.companyName,
                 password: data.password,
+                siteName: data.siteName,
             }),
         });
 
@@ -42,6 +43,7 @@ const Page = () => {
             <h2 className="text-xl text-center">Регистрация</h2>
             <Input {...register('login')} placeholder="E-mail" required type="email"/>
             <Input {...register('companyName')} placeholder="Название компании" required type="text"/>
+            <Input {...register('siteName')} placeholder="Название сайта" required type="text"/>
             <Input {...register('password')} placeholder="Пароль" required type="password"/>
             <Button>Зарегестрироватся</Button>
             <p className='text-center'>Есть аккаунт? <Link href={"/admin/login"}><span
