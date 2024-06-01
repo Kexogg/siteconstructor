5c8f9c6b056e76dccfda95c4ab086202550efc4c
kexogg <kexogg@gmail.com>
2024-06-02 04:02:31 +0500

    db reset

--- server/SiteConstructor.Infrastructure/Persistence/DatabaseContext.cs
+++ server/SiteConstructor.Infrastructure/Persistence/DatabaseContext.cs
@@ -13,6 +13,7 @@ public sealed class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions<DatabaseContext> options)
         : base(options)
     {
+        Database.EnsureDeleted();
         Database.Migrate();
         Database.EnsureCreated();
     }
