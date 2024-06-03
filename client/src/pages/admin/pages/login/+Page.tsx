02c6a09d1a14371b6f49c20f5afa8ba1f06704b7
kexogg <kexogg@gmail.com>
2024-06-04 03:22:28 +0500

    fix api url

--- client/src/pages/admin/pages/login/+Page.tsx
+++ client/src/pages/admin/pages/login/+Page.tsx
@@ -1,49 +1,62 @@
 import Button from "../../../../components/Button/Button";
-import {Link} from "../../../../components/Link";
+import { Link } from "../../../../components/Link";
 import Input from "../../../../components/Input/Input";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {useState} from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Page = () => {
-    const [error, setError] = useState<string | null>(null);
-    interface Inputs {
-        login: string,
-        password: string
+  const [error, setError] = useState<string | null>(null);
+  interface Inputs {
+    login: string;
+    password: string;
+  }
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        login: data.login,
+        password: data.password,
+      }),
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      setError("Ошибка авторизации");
     }
-
-    const { register, handleSubmit } = useForm<Inputs>();
-
-    const onSubmit: SubmitHandler<Inputs> = async (data) => {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                login: data.login,
-                password: data.password,
-            }),
-        });
-
-        if (response.ok) {
-            window.location.reload();
-        } else {
-            setError('Ошибка авторизации');
-        }
-    };
-    return (
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
-            <h2 className="text-xl text-center">Авторизация</h2>
-            <Input {...register('login')} placeholder="E-mail" required type="email"/>
-            <Input {...register('password')} placeholder="Пароль" required type="password"/>
-            <Button>Войти</Button>
-            <p className='text-center'>Нет аккаунта? <Link href={"/admin/register"}><span
-                className="text-blue-500 underline">Регистрация</span></Link>
-            </p>
-            {error && <p className="text-red-500 text-center">{error}</p>}
-        </form>
-    );
+  };
+  return (
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
+      <h2 className="text-xl text-center">Авторизация</h2>
+      <Input
+        {...register("login")}
+        placeholder="E-mail"
+        required
+        type="email"
+      />
+      <Input
+        {...register("password")}
+        placeholder="Пароль"
+        required
+        type="password"
+      />
+      <Button>Войти</Button>
+      <p className="text-center">
+        Нет аккаунта?{" "}
+        <Link href={"/admin/register"}>
+          <span className="text-blue-500 underline">Регистрация</span>
+        </Link>
+      </p>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+    </form>
+  );
 };
 
 export default Page;
--- client/src/pages/admin/pages/register/+Page.tsx
+++ client/src/pages/admin/pages/register/+Page.tsx
@@ -1,57 +1,80 @@
-import {SubmitHandler, useForm} from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import Input from "../../../../components/Input/Input";
 import Button from "../../../../components/Button/Button";
-import {Link} from "../../../../components/Link";
-import {useState} from "react";
+import { Link } from "../../../../components/Link";
+import { useState } from "react";
 
 const Page = () => {
-    const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
-    interface Inputs {
-        login: string,
-        siteName: string,
-        companyName: string,
-        password: string
+  interface Inputs {
+    login: string;
+    siteName: string;
+    companyName: string;
+    password: string;
+  }
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const response = await fetch("/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        login: data.login,
+        orgName: data.companyName,
+        password: data.password,
+        siteName: data.siteName,
+      }),
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      setError("Ошибка регистрации");
     }
+  };
 
-    const {register, handleSubmit} = useForm<Inputs>();
-
-    const onSubmit: SubmitHandler<Inputs> = async (data) => {
-        const response = await fetch('/api/users/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                login: data.login,
-                orgName: data.companyName,
-                password: data.password,
-                siteName: data.siteName,
-            }),
-        });
-
-        if (response.ok) {
-            window.location.reload();
-        } else {
-            setError('Ошибка регистрации');
-        }
-    };
-
-    return (
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
-            <h2 className="text-xl text-center">Регистрация</h2>
-            <Input {...register('login')} placeholder="E-mail" required type="email"/>
-            <Input {...register('companyName')} placeholder="Название компании" required type="text"/>
-            <Input {...register('siteName')} placeholder="Название сайта" required type="text"/>
-            <Input {...register('password')} placeholder="Пароль" required type="password"/>
-            <Button>Зарегестрироватся</Button>
-            <p className='text-center'>Есть аккаунт? <Link href={"/admin/login"}><span
-                className="text-blue-500 underline">Войти</span></Link>
-            </p>
-            {error && <p className="text-red-500 text-center">{error}</p>}
-        </form>
-    );
+  return (
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-2xl font-bold text-center">ExpoBuilder</h1>
+      <h2 className="text-xl text-center">Регистрация</h2>
+      <Input
+        {...register("login")}
+        placeholder="E-mail"
+        required
+        type="email"
+      />
+      <Input
+        {...register("companyName")}
+        placeholder="Название компании"
+        required
+        type="text"
+      />
+      <Input
+        {...register("siteName")}
+        placeholder="Название сайта"
+        required
+        type="text"
+      />
+      <Input
+        {...register("password")}
+        placeholder="Пароль"
+        required
+        type="password"
+      />
+      <Button>Зарегестрироватся</Button>
+      <p className="text-center">
+        Есть аккаунт?{" "}
+        <Link href={"/admin/login"}>
+          <span className="text-blue-500 underline">Войти</span>
+        </Link>
+      </p>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+    </form>
+  );
 };
 
 export default Page;
