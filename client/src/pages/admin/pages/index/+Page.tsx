7f71b8611bcc10d4ea766cfb583128166c4f78f3
kexogg <kexogg@gmail.com>
2024-06-03 20:27:00 +0500

    update homepage admin

--- client/src/pages/admin/pages/index/+Page.tsx
+++ client/src/pages/admin/pages/index/+Page.tsx
@@ -1,19 +1,22 @@
-
 import AdminPageContainer from "../../../../components/Admin/AdminPageContainer/AdminPageContainer";
-import {Data} from "./+data";
-import {useData} from "vike-react/useData";
+import { Data } from "./+data";
+import { useData } from "vike-react/useData";
 
 const Page = () => {
-    //throw redirect("/admin/login")
-    const data = useData<Data>()
-    console.log(data)
-    return (
-        <AdminPageContainer title="Главная">
-            <p>ID: {data.id}</p>
-            <p>Email: {data.login}</p>
-            <p>Организация: {data.orgName}</p>
-        </AdminPageContainer>
-    )
-}
+  const data = useData<Data>();
+  return (
+    <AdminPageContainer title="Главная">
+      <p>ID: {data.id}</p>
+      <p>Email: {data.login}</p>
+      <p>Организация: {data.orgName}</p>
+      <p>
+        Ваш сайт:{" "}
+        <a className={"underline"} href={`/expo/${data.siteName}/`}>
+          {data.siteName}
+        </a>
+      </p>
+    </AdminPageContainer>
+  );
+};
 
 export default Page;
