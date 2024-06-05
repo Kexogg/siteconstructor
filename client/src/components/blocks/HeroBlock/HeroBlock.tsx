330fa0ca46ce019c868294dd1b77c836cd5a8b09
kexogg <kexogg@gmail.com>
2024-06-06 03:13:52 +0500

    update styles

--- client/src/components/blocks/HeroBlock/HeroBlock.tsx
+++ client/src/components/blocks/HeroBlock/HeroBlock.tsx
@@ -1,16 +1,19 @@
-import {HeroBlockProps} from "../../../types/blocks";
+import { HeroBlockProps } from "../../../types/blocks";
 
-const HeroBlock = ({data}: { data: HeroBlockProps }) => {
-    return (
-        <section className={'h-96 flex'}>
-            <img src={data.jsonb.background} alt={data.jsonb.header}
-                 className={'absolute object-cover w-full h-96 brightness-50'}/>
-            <div className={'container m-auto z-10 text-white px-3'}>
-                <h2 className={'text-4xl'}>{data.jsonb.header}</h2>
-                {data.jsonb.text}
-            </div>
-        </section>
-    );
+const HeroBlock = ({ data }: { data: HeroBlockProps }) => {
+  return (
+    <section className={"h-96 flex"}>
+      <img
+        src={data.jsonb.background}
+        alt={data.jsonb.header}
+        className={"absolute object-cover w-full h-96 brightness-50"}
+      />
+      <div className={"container m-auto z-10 px-3"}>
+        <h2 className={"text-4xl"}>{data.jsonb.header}</h2>
+        {data.jsonb.text}
+      </div>
+    </section>
+  );
 };
 
 export default HeroBlock;
--- client/src/components/blocks/IFrameBlock/IFrameBlock.tsx
+++ client/src/components/blocks/IFrameBlock/IFrameBlock.tsx
@@ -1,18 +1,24 @@
-import {IFrameBlockProps} from "../../../types/blocks";
+import { IFrameBlockProps } from "../../../types/blocks";
 
-const IFrameBlock = ({data}: { data: IFrameBlockProps }) => {
-    return (
-        <section className={'h-96 flex'}>
-            <div className={'container mx-auto flex py-5 justify-center flex-col items-center gap-5'}>
-                <h2 className={'text-4xl text-center'}>{data.jsonb.title}</h2>
-                <div className={''}>
-                    <iframe src={data.jsonb.src} className={'h-72 aspect-video'} title={data.jsonb.title}/>
-                </div>
-            </div>
-
-        </section>
-    );
+const IFrameBlock = ({ data }: { data: IFrameBlockProps }) => {
+  return (
+    <section className={"h-96 flex"}>
+      <div
+        className={
+          "container mx-auto flex py-5 justify-center flex-col items-center gap-5"
+        }
+      >
+        <h2 className={"text-4xl text-center"}>{data.jsonb.title}</h2>
+        <div className={""}>
+          <iframe
+            src={data.jsonb.src}
+            className={"h-72 aspect-video"}
+            title={data.jsonb.title}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
-
 
 export default IFrameBlock;
