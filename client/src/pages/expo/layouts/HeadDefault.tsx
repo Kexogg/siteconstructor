493547b95df3a58fcd120e2f6d79a337211a3ee8
kexogg <kexogg@gmail.com>
2024-06-08 02:13:09 +0500

    google fonts

--- client/src/pages/expo/layouts/HeadDefault.tsx
+++ client/src/pages/expo/layouts/HeadDefault.tsx
@@ -1,13 +1,34 @@
-import {usePageContext} from "vike-react/usePageContext";
-import {PageConfig} from "../../../types/types";
+import { usePageContext } from "vike-react/usePageContext";
+import { PageConfig } from "../../../types/types";
 
 export default function HeadDefault() {
-    const context = usePageContext()
-    return (
-        <>
-            <title>{(context.data as PageConfig).title}</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <meta name="description" content={(context.data as PageConfig).description}/>
-        </>
-    );
+  const context = usePageContext();
+  return (
+    <>
+      <title>{(context.data as PageConfig).title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content={(context.data as PageConfig).description}
+      />
+      {context.site.styles?.fontFamily && (
+        <link
+          rel="stylesheet"
+          href={`https://fonts.googleapis.com/css?family=${context.site.styles.fontFamily.replace(
+            " ",
+            "+",
+          )}&display=swap`}
+        />
+      )}
+      {context.site.styles?.fontFamilyHeaders && (
+        <link
+          rel="stylesheet"
+          href={`https://fonts.googleapis.com/css?family=${context.site.styles.fontFamilyHeaders.replace(
+            " ",
+            "+",
+          )}&display=swap`}
+        />
+      )}
+    </>
+  );
 }
