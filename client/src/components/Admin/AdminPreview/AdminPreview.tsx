88578391056b3291c93b4ea37e05ba8cee034017
kexogg <kexogg@gmail.com>
2024-06-12 19:39:08 +0500

    fix z index

--- client/src/components/Admin/AdminPreview/AdminPreview.tsx
+++ client/src/components/Admin/AdminPreview/AdminPreview.tsx
@@ -8,7 +8,7 @@ type AdminPreviewProps = {
 const AdminPreview = ({children, title}: AdminPreviewProps) => {
     return (
         <details className={'relative bg-white border rounded-xl open:rounded-b-none max-h-96 overflow-y-scroll'}>
-            <summary className={'p-3 sticky top-0 bg-white z-40'}>{title}</summary>
+            <summary className={'p-3 sticky top-0 bg-white z-30'}>{title}</summary>
             {children}
         </details>
     );
