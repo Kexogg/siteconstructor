08f070566515b25fe0e81747e25ce28f8d544f37
kexogg <kexogg@gmail.com>
2024-06-16 03:04:47 +0500

    fix styles

--- client/src/components/Admin/AdminPreview/AdminPreview.tsx
+++ client/src/components/Admin/AdminPreview/AdminPreview.tsx
@@ -7,7 +7,7 @@ type AdminPreviewProps = {
 
 const AdminPreview = ({children, title}: AdminPreviewProps) => {
     return (
-        <details className={'bg-white border rounded-xl '}>
+        <details className={'bg-white outline outline-1 outline-neutral-400 rounded-xl open:rounded-b-none'}>
             <summary className={'p-3 bg-white'}>{title}</summary>
             <div className={'max-h-96 overflow-y-scroll relative'}>
                 {children}
